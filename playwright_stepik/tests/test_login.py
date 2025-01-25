7457c3525ddcc24791485a7a73233734665ae483
Anna Fedorova <anna.fedorova.testing@gmail.com>
2025-01-25 20:56:07 +0300

    update tests from lesson 6 (Allure report)

--- playwright_stepik/tests/test_login.py
+++ playwright_stepik/tests/test_login.py
@@ -1,19 +1,32 @@
 import pytest
+import allure
 
 
+@allure.feature('Authorization')
+@allure.story('Login Feature')
+@allure.title('Authorization with wrong credentials')
+@allure.severity(allure.severity_level.CRITICAL)
 def test_login_failure(login_page):
-    login_page.navigate()
-    login_page.login('invalid_user', 'invalid_password')
+    with allure.step('Open login page'):
+        login_page.navigate()
+    with allure.step('Enter invalid credentials in the authorization form'):
+        login_page.login('invalid_user', 'invalid_password')
+    with allure.step('The error is displayed - Invalid credentials. Please try again'):
+        assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
-    assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
-
+@allure.feature('Authorization')
+@allure.story('Login Feature')
+@allure.title('Authorization with valid credentials')
+@allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.parametrize('username, password', [
     ('user', 'user'),
     ('admin', 'admin')
 ])
 def test_login_success(login_page, dashboard_page, username, password):
-    login_page.navigate()
-    login_page.login(username, password)
-
-    dashboard_page.assert_welcome_message(f'Welcome {username}')
+    with allure.step('Open login page'):
+        login_page.navigate()
+    with allure.step('Enter valid credentials in the authorization form'):
+        login_page.login(username, password)
+    with allure.step("A welcome message with the user's name is displayed."):
+        dashboard_page.assert_welcome_message(f'Welcome {username}')
